8542493693aba354cf000687b34a6bf51e4a5ef0
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2024-04-22 13:26:33 +0200

    mainPage js

---
+++ client/mainPage.js
@@ -0,0 +1,7 @@
+function createMainPage() {
+  const introContainer = document.createElement('div');
+  introContainer.id = 'introContainer';
+  document.body.append(introContainer);
+}
+
+export default createMainPage;
--- client/script.js
+++ client/script.js
@@ -1,1 +1,14 @@
-console.log('hello');
+import createMainPage from './mainPage.js';
+
+const url = window.location.href;
+
+console.log(createMainPage);
+function getPages() {
+  switch (url) {
+    case 'http://localhost:4000/':
+      createMainPage();
+      break;
+  }
+}
+
+getPages();
